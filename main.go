1c20dc5ea97f5a90c50b640f523098ce927cfd60
ivan <ivan@ivan.ivan>
2024-07-24 16:18:41 +0300

    Enhanced CLI

--- main.go
+++ main.go
@@ -5,79 +5,89 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
+	searchFlag := flag.String("search", "", "Search anime title. Pass this to see if an anime can be played.")
+	selectFlag := flag.Int("select", 0, "Select anime. Specify the number of the desired anime.")
+	episodeFlag := flag.Int("episode", 0, "Select an episode you want to watch.")
+	videoFlag := flag.Int("video", 0, "Select the voiceover you want to get url of.")
+
 	flag.Parse()
 
-	switch {
-	case len(flag.Args()) == 0:
-		fmt.Printf("No arguments specified!\nTry: search, episodes, watch\n")
-		os.Exit(0)
-	case len(flag.Args()) > 2:
-		fmt.Printf("Too many arguments specified!\n")
+	if *searchFlag == "" {
+		fmt.Printf("No title specified!\n")
 		os.Exit(0)
 	}
 
-	switch flag.Arg(0) {
-	case "search":
-		fmt.Printf("Searching %s...\n", flag.Arg(1))
+	ar, err := anilib.Search(*searchFlag)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
 
-		resp, err := anilib.Search(flag.Arg(1))
-		if err != nil {
-			fmt.Printf("%v", err)
-			os.Exit(1)
+	if *selectFlag == 0 {
+		for i, el := range ar.Data {
+			fmt.Printf("%d. %s\n", i+1, el.RusName)
 		}
+		os.Exit(0)
+	}
 
-		for i, el := range resp.Data {
-			fmt.Printf("%d. %s / slug_url: %s\n", i+1, el.Name, el.SlugUrl)
-		}
+	animeList := make(map[int]string)
+	for i, el := range ar.Data {
+		animeList[i+1] = el.SlugUrl
+	}
 
-		fmt.Printf("\nNext use \"anilib-cli episodes <slug_url>\"\n")
-	case "episodes":
-		fmt.Printf("Getting episodes of %s...\n", flag.Arg(1))
+	ep, err := anilib.GetEpisodes(animeList[*selectFlag])
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
 
-		resp, err := anilib.GetEpisodes(flag.Arg(1))
-		if err != nil {
-			fmt.Printf("%v", err)
-			os.Exit(1)
-		}
-
-		for i, el := range resp.Episodes {
+	if *episodeFlag == 0 {
+		for i, el := range ep.Episodes {
 			if el.Name != "" {
-				fmt.Printf("%d. %s. ID: %d\n", i+1, el.Name, el.ID)
+				fmt.Printf("%d. %s\n", i+1, el.Name)
 			} else {
-				fmt.Printf("%d. ID: %d\n", i+1, el.ID)
+				fmt.Printf("%d. Episode\n", i+1)
 			}
 		}
+		os.Exit(0)
+	}
 
-		fmt.Printf("\nNext use \"anilib-cli watch <ID>\"\n")
-	case "watch":
-		fmt.Printf("Getting teams of %s\n", flag.Arg(1))
+	episodeList := make(map[int]int)
+	for i, el := range ep.Episodes {
+		episodeList[i+1] = el.ID
+	}
 
-		id, err := strconv.Atoi(flag.Arg(1))
-		if err != nil {
-			fmt.Printf("%v", err)
-			os.Exit(1)
-		}
+	vid, err := anilib.GetTeams(episodeList[*episodeFlag])
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
 
-		resp, err := anilib.GetTeams(id)
-		if err != nil {
-			fmt.Printf("%v", err)
-			os.Exit(1)
-		}
-
-		fmt.Printf("Available teams of %s:\n", resp.Data.Name)
-		baseUrl := "https://video1.anilib.me/.%D0%B0s/"
-		for _, el := range resp.Data.Players {
+	ctr := 1
+	if *videoFlag == 0 {
+		for _, el := range vid.Data.Players {
 			if el.Player == "Animelib" {
 				for _, q := range el.Video.Quality {
-					fmt.Printf("%s - %s%s [%dp]\n", el.Team.Name, baseUrl, q.HREF, q.Quality)
+					fmt.Printf("%d. %s [%dp]\n", ctr, el.Team.Name, q.Quality)
+					ctr += 1
 				}
 			}
 		}
-	default:
-		fmt.Printf("Invalid argument!\n")
 	}
+
+	ctr = 1
+	baseUrl := "https://video1.anilib.me/.%D0%B0s/"
+	videoList := make(map[int]string)
+	for _, el := range vid.Data.Players {
+		if el.Player == "Animelib" {
+			for _, q := range el.Video.Quality {
+				videoList[ctr] = baseUrl + q.HREF
+				ctr += 1
+			}
+		}
+	}
+
+	fmt.Fprintf(os.Stdout, "%s", videoList[*videoFlag])
 }
